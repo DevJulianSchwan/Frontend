88dde5c8135bad6f6d3a85def7d89b4ea4aa1c03
jayfore <jayfore@users.noreply.github.com>
2020-08-06 23:09:23 +0200

    Die nächsten ToDos sind als Kommentar hinterlegt.

--- Frontend/Form1.cs
+++ Frontend/Form1.cs
@@ -50,6 +50,29 @@ namespace Frontend
                 //Picturebox1.Image = New Bitmap("Image Path")
 
                 //set_Image(pictureBox1, filepath);
+
+
+
+
+
+                /*
+                 * Potentielle next steps:
+                 * A) der Open File Dialog sollte nur Bilddateien anzeigen und auch nur diese öffnen können. Vllt hilfreich:
+                 * https://www.c-sharpcorner.com/UploadFile/mahesh/openfiledialog-in-C-Sharp/#:~:text=C%23%20OpenFileDialog%20control%20allows%20us,folders%20and%20select%20a%20file.
+                 * 
+                 * B) die mit dem Open FIle dialog geöffnete bilddatei sollte in der Picture Box angezeigt werden. Dafür muss verstanden werden wie ein beliebiges Bild allgemein in
+                 * die Picture Box geladen werden kann.
+                 * 
+                 * C) wenn B erfolgt ist kann man damit anfangen Rechtecke auf das User Interface zu malen um die Blüten zu markieren
+                 * 
+                 * D) als potentielle Übung: Erstelle eine Methode ~name(Input) die eine .csv Datei irgendwo lokal auf deinem Rechner speichert
+                 * 
+                 * E) Das Trello Board sollte mal zum laufen gebracht werden.
+                 */
+
+
+
+
             }
         }
 
